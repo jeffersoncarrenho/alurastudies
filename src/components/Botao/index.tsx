1995b52df746f33556b94ae52bc9c0a7943a0eec
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-09-20 11:32:10 -0300

    Aula 3

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component {
   render(): React.ReactNode {
-    return <button>Botão</button>;
+    return <button className={style.botao}>Botão</button>;
   }
 }
 
