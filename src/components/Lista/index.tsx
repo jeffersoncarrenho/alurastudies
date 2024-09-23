e1a82077dc68e6612c1429ec97897be221f9b1a7
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-09-23 16:07:33 -0300

    Aula 5

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,12 +1,8 @@
-import React from "react";
 import style from "./Lista.module.scss";
 import Item from "./Item";
-function Lista() {
-  const tarefas = [
-    { tarefa: "React", tempo: "02:00:00" },
-    { tarefa: "Javascript", tempo: "01:00:00" },
-    { tarefa: "Typescript", tempo: "03:00:00" },
-  ];
+import { ITarefa } from "../../types/tarefa";
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
