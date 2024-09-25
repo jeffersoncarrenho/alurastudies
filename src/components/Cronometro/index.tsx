db5f7123ca68ade2164824f76322187b18c4f048
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-09-25 11:19:57 -0300

    Aula 6

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,26 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  });
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <p>Tempo: {tempo}</p>
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
